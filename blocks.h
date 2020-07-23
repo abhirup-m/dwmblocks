1342c74cd2903eac6b84c7ad5f5c1678fea98343
SearyBlue <mukherjeeabhirup44@gmail.com>
2020-07-23 11:20:10 +0530

    brought back colored emoji because libxft-bgra-git builds

--- blocks.h
+++ blocks.h
@@ -2,8 +2,8 @@ static const Block blocks[] = {
 	{"", "~/.local/bin/dwmblock_scripts/music-show", 0, 11},
 	{"", "volume -s 2> /dev/null &> /dev/null &", 0, 8},
 	{" ", "~/.local/bin/dwmblock_scripts/wifi-show", 0, 10},
-	{"  ", "date '+%b %d,%a'", 300, 0},
-	{"  ", "date '+%I:%M %p'", 60, 9},
+	{" 🗓 ", "date '+%b %d,%a'", 300, 0},
+	{" ⏰ ", "date '+%I:%M %p'", 60, 9},
 	{" ", "~/.local/bin/dwmblock_scripts/battery-show", 300, 9},
 };
 static char delim = ' ';
